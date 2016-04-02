0d7edf89626604fb22a1b1454dfd2b08deefed9e
ScientistJeff <user@server.fake>
2016-04-02 11:08:34 -0400

    Cleaned up directory

--- src/bubbledoc.R
+++ src/bubbledoc.R
@@ -1,11 +1,17 @@
+#Bubble charts
+
+
 setwd("~/Data_Projects/visualizations")
-bub <- read.csv("BubbleData.csv")
-
-
+#Read in data
+bub <- read.csv("data/BubbleData.csv")
+#simple Plot
+plot(bub$Precision,bub$Recall,xlab="Precision", ylab="Recall" )
+text(bub$Precision, bub$Recall+.03, bub$ID, cex=0.5)
 #we want to scale the bubbles by area, not radius
 radius = sqrt( bub$Number.of.Records/ pi )
 
 
+#Bubble Chart Plot 
 symbols(bub$Precision,bub$Recall,circles=radius,
         inches=0.15, fg="white", bg=(bub$Category+1), xlab="Precision", ylab="Recall")
 text(bub$Precision, bub$Recall, bub$ID, cex=0.5)
